968bb46e0625c8932f2c9c5559348e3de67d0555
“Tushar <tushar.kolhe01@gmail.com>
2018-04-13 08:30:15 +0530

    Implementation of Datasourse methods

---
+++ TKTableView/CustomCell.swift
@@ -0,0 +1,25 @@
+//
+//  CustomCell.swift
+//  TKTableView
+//
+//  Created by Tushar Kolhe on 12/04/18.
+//  Copyright © 2018 Tushar Kolhe. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TKTableView/ViewController.swift
+++ TKTableView/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    var sportsArray = ["Football","Cricket","Hockey","Tennis","Volleyball","Table Tennis","Golf"]
+
+    // cell reuse id (cells that scroll out of view can be reused)
+    let cellReuseIdentifier = "cell"
+    
+    // don't forget to hook this up from the storyboard
+    @IBOutlet var tableView: UITableView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Register the table view cell class and its reuse id
+//        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        
+        // (optional) include this line if you want to remove the extra empty cell divider lines
+        // self.tableView.tableFooterView = UIView()
+        
+        // This view controller itself will provide the delegate methods and row data for the table view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +40,30 @@ class ViewController: UIViewController {
     }
 
 
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sportsArray.count
+    }
+    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//        // create a new cell if needed or reuse an old one
+//        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+//
+//        // set the text from the data model
+//        cell.textLabel?.text = self.sportsArray[indexPath.row]
+//
+//        return cell
+//    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! CustomCell
+        
+        let text = sportsArray[indexPath.row]
+        
+        cell.nameLabel.text = text
+        
+        return cell
+    }
 }
 
